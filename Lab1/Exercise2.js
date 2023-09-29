7091e2d1601a3c31d5dfda76f3a6ab8f5335e72f
Oisin Hearne <oisinhearne@gmail.com>
2023-09-29 14:34:11 +0100

    Part 2B and 2C, taking paramaters into an arrow function.

--- Lab1/Exercise2.js
+++ Lab1/Exercise2.js
@@ -4,7 +4,25 @@
 //into it.
 const sampleString = "Data Representation & Querying"
 const say = (x) => {console.log(x)};
-say(sampleString);
+say(sampleString); //Result: "Data Representation & Querying"
 
 // 2. (b)
+//Arrow function now takes a parameter and does something
+//to it, then returns it.
+//In this case, sampleNumber is passed in, printed, then incremented & returned.
+const sampleNumber = 3;
+const increment = (x) => {
+    console.log(x); //Result: 3
+    return ++x; 
+};
+console.log(increment(sampleNumber)); //Result: 4
 
+// 2. (c)
+//Arrow function takes two params, sums them, prints them, then returns them.
+const sampleSum1 = 2;
+const sampleSum2 = 3;
+const sum = (x, y) => {
+    console.log(x+y); //Result: 5
+    return x+y;
+}
+sum(sampleSum1, sampleSum2);
