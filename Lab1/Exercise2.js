c9e24983290fd28be7073eaa0afd62d8bcfaf0cc
Oisin Hearne <oisinhearne@gmail.com>
2023-09-29 14:25:23 +0100

    Part 2 (a), Arrow Function w/ argument.

---
+++ Lab1/Exercise2.js
@@ -0,0 +1,10 @@
+// 2. (a)
+//Arrow function to print "Data Representation & Querying" in console.
+//Arrow function takes an argument, in this case sampleString is passed
+//into it.
+const sampleString = "Data Representation & Querying"
+const say = (x) => {console.log(x)};
+say(sampleString);
+
+// 2. (b)
+
