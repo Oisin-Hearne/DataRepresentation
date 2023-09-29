63708ef0618add761fd3d6f08361f190d5f0f067
Oisin Hearne <oisinhearne@gmail.com>
2023-09-29 14:47:01 +0100

    3C completed, listTasks.

--- Lab1/Exercise3.js
+++ Lab1/Exercise3.js
@@ -1,6 +1,6 @@
 // 3. (a)
 // an array of strings
-const stringArray = ["hello", "today", "is", "the", "twenty-seventh", "of"];
+const stringArray = ["Hello,", "today", "is", "the", "twenty-seventh", "of"];
 
 // 3. (b)
 // addTask function uses the array.push function to add
@@ -13,3 +13,12 @@ const addTask = (insert) => {
 const month = "September";
 console.log("Elements in stringArray after adding: "+addTask(month));
 
+// 3. (c)
+// Iterates through every item in the array
+// and prints every item to console.
+const listTasks = () => {
+    stringArray.forEach(element => {
+        console.log(" -> "+ element);
+    });
+};
+listTasks();
