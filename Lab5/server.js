40201f85144bccb8ab4503f0fbac1e2f49785433
Oisin Hearne <oisinhearne@gmail.com>
2023-10-27 09:45:48 +0100

    L5: Params in URL Added.

--- Lab5/server.js
+++ Lab5/server.js
@@ -3,9 +3,20 @@ const app = express()
 const port = 4000
 
 app.get('/', (req, res) => {
-  res.send('Welcome to Data Representation & Querying !')
+    res.send('Welcome to Data Representation & Querying !')
+})
+
+app.get('/hello/:name', (req, res) => {
+    res.send('Hello, '+req.params.name)
+})
+
+app.get('/goodbye/:name', (req, res) => {
+    res.send('Goodbye, '+req.params.name)
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
+})
+
+app.get('/api/books', (req, res) => {
 })
