695ac04f3156b3299b7fa388ead9e8dae06f2ebb
Oisin Hearne <oisinhearne@gmail.com>
2023-10-27 10:32:34 +0100

    L5: Added Post Method to form. (& body-parser)

--- Lab5/server.js
+++ Lab5/server.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 4000
 const data = {"books":[{"title":"Learn Git in a Month of Lunches","isbn":"1617292419","pageCount":0,"thumbnailUrl":"https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/umali.jpg","status":"MEAP","authors":["Rick Umali"],"categories":[]},{"title":"MongoDB in Action, Second Edition","isbn":"1617291609","pageCount":0,"thumbnailUrl":"https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/banker2.jpg","status":"MEAP","authors":["Kyle Banker","Peter Bakkum","Tim Hawkins","Shaun Verch","Douglas Garrett"],"categories":[]},{"title":"Getting MEAN with Mongo, Express, Angular, and Node","isbn":"1617292036","pageCount":0,"thumbnailUrl":"https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/sholmes.jpg","status":"MEAP","authors":["Simon Holmes"],"categories":[]}]}
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 
 app.get('/', (req, res) => {
     res.send('Welcome to Data Representation & Querying !')
@@ -30,12 +34,14 @@ app.get('/test', (req, res) => {
     res.sendFile(__dirname+"/index.html");
 })
 
-//This takes data from a get/post and reshows it
+//This takes data from a get method and reshows it
 app.get('/name', (req, res) => {
-    res.send('Hello, '+req.query.fname + " " + req.query.lname);
+    res.send('Hello from a get, '+req.query.fname + " " + req.query.lname);
 })
 
-
+app.post('/name', (req, res) => {
+    res.send('Hello from a post, '+req.body.fname + " " + req.body.lname);
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
