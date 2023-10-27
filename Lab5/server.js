ac4dd06b99b07dd8bb6e1e7ba01765175cef14b5
Oisin Hearne <oisinhearne@gmail.com>
2023-10-27 09:51:55 +0100

    L5: /api/books now returns a JSON

--- Lab5/server.js
+++ Lab5/server.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 4000
+const data = {"books":[{"title":"Learn Git in a Month of Lunches","isbn":"1617292419","pageCount":0,"thumbnailUrl":"https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/umali.jpg","status":"MEAP","authors":["Rick Umali"],"categories":[]},{"title":"MongoDB in Action, Second Edition","isbn":"1617291609","pageCount":0,"thumbnailUrl":"https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/banker2.jpg","status":"MEAP","authors":["Kyle Banker","Peter Bakkum","Tim Hawkins","Shaun Verch","Douglas Garrett"],"categories":[]},{"title":"Getting MEAN with Mongo, Express, Angular, and Node","isbn":"1617292036","pageCount":0,"thumbnailUrl":"https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/sholmes.jpg","status":"MEAP","authors":["Simon Holmes"],"categories":[]}]}
 
 app.get('/', (req, res) => {
     res.send('Welcome to Data Representation & Querying !')
 })
 
+//:name is a paramater, and can be accessed via req.params.name.
+//This means that we can change what appears on the webpage based on what path the user
+//puts into the URL. This'll send back whatever the user puts into it.
 app.get('/hello/:name', (req, res) => {
     res.send('Hello, '+req.params.name)
 })
@@ -14,9 +18,15 @@ app.get('/goodbye/:name', (req, res) => {
     res.send('Goodbye, '+req.params.name)
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+//This lets our webapp act like an API - it returns JSON!
+//We can now use localhost:4000/api/books instead of the URL we were previously using to fetch books.
+//You can use jsonlint to compress the JSON into a single line.
+app.get('/api/books', (req, res) => {
+    res.json(data);
 })
 
-app.get('/api/books', (req, res) => {
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
 })
